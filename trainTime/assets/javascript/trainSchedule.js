298b511f317abc9c3fe8b10008b4d1018043ff80
Kristi Heredia <froglander@dukworld.net>
2016-08-11 16:05:55 -0400

    Added parsley for validation, added an image, added delete capability

--- trainTime/assets/javascript/trainSchedule.js
+++ trainTime/assets/javascript/trainSchedule.js
@@ -1,32 +1,57 @@
- // Initialize Firebase
+/* ************************************************************	*/
+/* Initialize Firebase 											*/
+/* ************************************************************	*/
 var config = {
 	apiKey: "AIzaSyBIbW49fs6vk9WPERYvWFYBLD0wgpHbyvc",
 	authDomain: "week7homework-2ec6b.firebaseapp.com",
 	databaseURL: "https://week7homework-2ec6b.firebaseio.com",
 	storageBucket: "week7homework-2ec6b.appspot.com",
 };
-
 firebase.initializeApp(config);
 
 // Create a variable to reference the database.
 var trainDatabase = firebase.database();
 
 
-// At the initial load, get a snapshot of the current data.
+/* ************************************************************	*/
+/* Function : on child_added									*/
+/* Parameters : childSnapshot									*/	
+/* Description : This function is part of the firebase library	*/
+/*				 and is triggered each time a child is added.	*/
+/*				 It will also be triggered when the page loads.	*/
+/*				 It is used to build up each row of train data 	*/
+/*				 and display it on the page						*/
+/* ************************************************************	*/
 trainDatabase.ref().on("child_added", function(childSnapshot) {
-	console.log(childSnapshot.val());
+	// console.log(childSnapshot.val());
 
+	// Build up variables useful for calculation how many minutes away
+	// the next train is	
+	var frequency = childSnapshot.val().frequency;
+	var firstTrainTime = moment(childSnapshot.val().firstTrainTime, "hh:mm").subtract(1, "years");	
+	// minutes away  = frequency - [minutes(currentTime - startTime) % frequency ]
+	var minAway = frequency - (moment().diff(moment(firstTrainTime), "minutes") % frequency);
+
+	// jQuery variable to reference the #trainRows id in html
 	var $trainBody = $('#trainRows');
-
-	var $trainRow = $('<tr>');
+	// Build a jquery object for the train row
+	var $trainRow = $('<tr>').attr('id', childSnapshot.key);
+	// Build a delete button to remove records from database
+	var $deleteBtn = $('<button>').append($('<span/>').addClass("glyphicon glyphicon-remove").attr('aria-hidden', "true"))
+								.on('click', function() { trainDatabase.ref(childSnapshot.key).remove();});
+							
+	// Build an edit button to edit records (button there, still need to add functionality)
+	var $editBtn = $('<button>').append($('<span/>').addClass("glyphicon glyphicon-pencil").attr('aria-hidden', "true"))
+								.on('click', function() { /* Do stuff here */ });
+	// Append the two buttons to a <td> element and append to the row
+	var $update = $('<td>').append($deleteBtn).append($editBtn).appendTo($trainRow);
 
 	var $trainName = $('<td>').html(childSnapshot.val().trainName).appendTo($trainRow);
 	var $destination = $('<td>').html(childSnapshot.val().destination).appendTo($trainRow);
 	var $frequency = $('<td>').html(childSnapshot.val().frequency).appendTo($trainRow);
-	var $nextArrival = $('<td>').html(childSnapshot.val().firstTrainTime).appendTo($trainRow);
-	var $minutesAway = $('<td>').html(childSnapshot.val().frequency).appendTo($trainRow);
-
-		
+	var $nextArrival = $('<td>').html(moment(moment().add(minAway, "minutes")).format("hh:mm")).appendTo($trainRow);
+	var $minutesAway = $('<td>').html(minAway).appendTo($trainRow);
+	// Append trainRow to #trainRows element
 	$trainRow.appendTo($trainBody);
 
 	// If any errors are experienced, log them to console. 
@@ -34,30 +59,56 @@ trainDatabase.ref().on("child_added", function(childSnapshot) {
 	  	console.log("The read failed: " + errorObject.code);
 });
 
-// Whenever a user clicks the click button
-$("#submitNewTrain").on("click", function() {	
-	console.log("trainName:", $('#trainNameInput').val().trim());
-	console.log("destination:", $('#destinationInput').val().trim());
-	console.log("firstTrainTime:", $('#firstTrainTimeInput').val().trim());
-	console.log("frequency:", $('#frequencyInput').val().trim());
+/* ************************************************************	*/
+/* Function : on child_removed									*/
+/* Parameters : childSnapshot									*/	
+/* Description : This function is part of the firebase library	*/
+/*				 and is triggered each time a child is removed.	*/
+/*				 It removes the row with the id of 				*/
+/*				 childSnapshot.key
+/* ************************************************************	*/
+trainDatabase.ref().on("child_removed", function(childSnapshot) {
+	$('#'+childSnapshot.key).remove();
+});
 
+/* ************************************************************	*/
+/* Function : parsley on field:validated						*/
+/* Parameters : none 											*/	
+/* Description : I included a javascript library called parsley	*/
+/*				 for validation purposes. It uses attributes	*/
+/*				 included in the <input> elements to trigger 	*/
+/*				 the appropriate valication						*/
+/* ************************************************************	*/
+$('#addTrainForm').parsley().on('field:validated', function() {
+	var ok = $('.parsley-error').length === 0;
+	$('.bs-callout-info').toggleClass('hidden', !ok);
+	$('.bs-callout-warning').toggleClass('hidden', ok);
+})
+/* ************************************************************	*/
+/* Function : on form submit									*/
+/* Parameters : none											*/
+/* Description : This is executed when you click the submit		*/
+/*				 button for the form and pushes the entered 	*/
+/*				 data to the firebase database 					*/
+/* ************************************************************	*/
+.on('form:submit', function() {
+	// Build the train object
 	var trainObj = {
 		trainName: $('#trainNameInput').val().trim(),
 		destination: $('#destinationInput').val().trim(),
-		firstTrainTime: $('#firstTrainTimeInput').val().trim(),
+		firstTrainTime: moment($('#firstTrainTimeInput').val().trim(), "hhmm").format("hh:mm")	,
 		frequency: $('#frequencyInput').val().trim()
 	}
+	// Push train object to database
 	trainDatabase.ref().push(trainObj);
 
-	return false;
-});
+	// Clear form values
+	$('#trainNameInput').val("");
+	$('#destinationInput').val("");
+	$('#firstTrainTimeInput').val("");
+	$('#frequencyInput').val("");
+	$('#trainNameInput').focus()
 
-
-// // When first loaded or when the connections list changes...
-// trainDatabase.on("value", function(snap) {
-
-//   // Display the viewer count in the html.
-//   // The number of online users is the number of children in the connections list.
-//   $("#watchers").html(snap.numChildren());
-
-// });
+	//So page doesn't refresh
+	return false; 
+}); 
